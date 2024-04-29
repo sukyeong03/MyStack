4dfb33813a6d97f86613733d6f85bc4732a9250c
sukyeong03 <pyosugyeong92@gmail.com>
2024-04-30 01:44:24 +0900

    add Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+#include "MyStack.h"
+
+int main(void) {
+    MyStack <int>intStack;
+    try{
+        intStack.push(100);
+        intStack.push(200);
+        cout << intStack.pop() << endl;
+        cout << intStack.pop() << endl;
+        cout << intStack.pop() << endl;
+    }
+    catch(const char* s) {
+        cout << "예외발생 : " << s << endl;
+    }
+}
