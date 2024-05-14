d6d1d0b8a9b8679dffbe415b10d722c9dce144e6
sukyeong03 <pyosugyeong92@gmail.com>
2024-05-14 11:09:16 +0900

    sort

--- Main.cpp
+++ Main.cpp
@@ -3,23 +3,30 @@ using namespace std;
 
 #include "MyStack.h"
 
-// 예외 처리를 담당하는 함수
-template <typename Func>
-void Exceptions(Func f) {
-    try {
-        f(); // 전달된 함수 실행
-    }  catch(const char* s) {
-        cout << "예외발생 : " << s << endl;
-    }
-} 
-
 int main(void) {
     MyStack<int> intStack; // 정수형 스택
     MyStack<char> charStack; // 문자형 스택
     
-    // 람다 함수를 사용하여 push 함수 호출을 래핑하여 전달
-    Exceptions([&]() {
-        intStack.push(0, 10);
-    });
+    intStack.push(30);
+    intStack.push(19);
+    intStack.push(41);
+    intStack.push(63);
+    intStack.push(3);
+    intStack.push(99);
+    intStack.push(83);
+    intStack.push(22);
+    intStack.push(77);
+    intStack.push(681);
+    intStack.push(492);
+    intStack.push(45);
+    intStack.printStack();
+
+    intStack.sort();
+    intStack.printStack();
+
+    
 
 }
+
+
+
